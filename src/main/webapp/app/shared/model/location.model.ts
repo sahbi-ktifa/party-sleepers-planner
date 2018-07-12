10f2e2bdb9f554e9cbca4951e11e0d48db6624da
sktifa <sahbi.ktifa@4dconcept.fr>
2018-07-12 14:39:10 +0200

    Fix locationDTO as FrontEnd component

--- src/main/webapp/app/shared/model/location.model.ts
+++ src/main/webapp/app/shared/model/location.model.ts
@@ -2,8 +2,9 @@ export interface ILocation {
     id?: number;
     name?: string;
     max?: number;
+    occupants?: number;
 }
 
 export class Location implements ILocation {
-    constructor(public id?: number, public name?: string, public max?: number) {}
+    constructor(public id?: number, public name?: string, public max?: number, public occupants?: number) {}
 }
